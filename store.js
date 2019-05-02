c564e23f8d132a842d966f292baff0dc2a6d37c6
Charlie Kim <cwkim3@gmail.com>
2019-05-02 17:11:33 -0400

    initial commit

---
+++ store.js
@@ -0,0 +1,42 @@
+const STORE = [
+	{
+		question: 'Which RHCP song was influenced by the death of Kurt Cobain?',
+		answers: ['Scar Tissue', 'Californication', 'Dani California', 'Tearjerker'],
+		correct: 'Tearjerker',
+		image: 'cobain.png',
+		alt: 'Kurt-Cobain-Image',
+		history: 'Tearjerker" was written about the life and death of Kurt Cobain and the effect he had on lead singer Anthony Kiedis'
+	},
+		{
+		question: 'What music festival in the late-90s did the RHCP play where a typhoon hit?',
+		answers: ['Coachella', 'Fyre Festival', 'Lollapalooza', 'Fuji Rock Festival'],
+		correct: 'Fuji Rock Festival',
+		image: 'fuji-rock.jpg',
+		alt: 'Fuji-Rock-Image',
+		history: 'The typhoon caused the band to end their set early, and the rest of the festival was cancelled.'
+	},
+	{
+		question: 'What popular animated TV show did the RHCP make a guest appearance playing "Give it Away" in their underwear?',
+		answers: ['Futurama', 'The Simpsons', 'South Park', 'Family Guy'],
+		correct: 'The Simpsons',
+		image: 'RHCP-Simpsons.jpg',
+		alt: 'RHCP-Simpsons-Image',
+		history: 'The Red Hot Chili Peppers are guest stars in the fourth season finale, “Krusty Gets Kancelled” where they sing Give It Away.'
+	},
+	{
+		question: 'Who is the legendary music producer behind all but 1 RHCP record?',
+		answers: ['Rick Rubin', 'Max Martin', 'Timbaland', 'Don Was'],
+		correct: 'Rick Rubin',
+		image: 'rick-rubin.jpeg',
+		alt: 'Rick-Rubin-Image',
+		history: 'Rubin took the RHCP\'s messy sound into more melodic territory, lashing their humping bass lines to fat metal riffs and the crackling rhythms of hip-hop.'
+	},
+	{
+		question: 'What was RHCP\'s first #1 album in the US?',
+		answers: ['Californication', 'Mother\'s Milk', 'By The Way', 'Stadium Arcadium'],
+		correct: 'Stadium Arcadium',
+		image: 'stadium-arcadium.jpg',
+		alt: 'Stadium-arcadium-album-cover',
+		history: 'Stadium Arcadium is the ninth studio album by American rock band Red Hot Chili Peppers. In the U.S., Stadium Arcadium became the band\'s first number one selling album.'
+	}
+]
