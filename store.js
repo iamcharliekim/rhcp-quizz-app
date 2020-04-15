318456f498942f434eb6bcf07c0d24ee56a694fb
Charlie Kim <cwkim3@gmail.com>
2020-04-15 18:41:07 -0400

    cleaned up code, styling, and added favicon

--- store.js
+++ store.js
@@ -1,32 +1,55 @@
 const STORE = [
-	{
-		question: 'Which RHCP song was influenced by the death of Kurt Cobain?',
-		answers: ['Scar Tissue', 'Californication', 'Dani California', 'Tearjerker'],
-		correct: 'Tearjerker',
-		history: 'Tearjerker" was written about the life and death of Kurt Cobain and the effect he had on lead singer Anthony Kiedis'
-	},
-		{
-		question: 'What music festival in the late-90s did the RHCP play where a typhoon hit?',
-		answers: ['Coachella', 'Fyre Festival', 'Lollapalooza', 'Fuji Rock Festival'],
-		correct: 'Fuji Rock Festival',
-		history: 'The typhoon caused the band to end their set early, and the rest of the festival was cancelled.'
-	},
-	{
-		question: 'What popular animated TV show did the RHCP make a guest appearance playing "Give it Away" in their underwear?',
-		answers: ['Futurama', 'The Simpsons', 'South Park', 'Family Guy'],
-		correct: 'The Simpsons',
-		history: 'The Red Hot Chili Peppers are guest stars in the fourth season finale, “Krusty Gets Kancelled” where they sing Give It Away.'
-	},
-	{
-		question: 'Who is the legendary music producer behind all but 1 RHCP record?',
-		answers: ['Rick Rubin', 'Max Martin', 'Timbaland', 'Don Was'],
-		correct: 'Rick Rubin',
-		history: 'Rubin took the RHCP\'s messy sound into more melodic territory, lashing their humping bass lines to fat metal riffs and the crackling rhythms of hip-hop.'
-	},
-	{
-		question: 'What was RHCP\'s first #1 album in the US?',
-		answers: ['Californication', 'Mother\'s Milk', 'By The Way', 'Stadium Arcadium'],
-		correct: 'Stadium Arcadium',
-		history: 'Stadium Arcadium is the ninth studio album by American rock band Red Hot Chili Peppers. In the U.S., Stadium Arcadium became the band\'s first number one selling album.'
-	}
-]
+  {
+    question: 'Which RHCP song was influenced by the death of Kurt Cobain?',
+    answers: [
+      'Scar Tissue',
+      'Californication',
+      'Dani California',
+      'Tearjerker',
+    ],
+    correct: 'Tearjerker',
+    history:
+      '"Tearjerker" was written about the life and death of Kurt Cobain and the effect he had on lead singer Anthony Kiedis',
+  },
+  {
+    question:
+      'What music festival in the late-90s did the RHCP play where a typhoon hit?',
+    answers: [
+      'Coachella',
+      'Fyre Festival',
+      'Lollapalooza',
+      'Fuji Rock Festival',
+    ],
+    correct: 'Fuji Rock Festival',
+    history:
+      'The typhoon caused the band to end their set early, and the rest of the festival was cancelled.',
+  },
+  {
+    question:
+      'What popular animated TV show did the RHCP make a guest appearance playing "Give it Away" in their underwear?',
+    answers: ['Futurama', 'The Simpsons', 'South Park', 'Family Guy'],
+    correct: 'The Simpsons',
+    history:
+      'The Red Hot Chili Peppers are guest stars in the fourth season finale, “Krusty Gets Kancelled” where they sing Give It Away.',
+  },
+  {
+    question:
+      'Who is the legendary music producer behind all but 1 RHCP record?',
+    answers: ['Rick Rubin', 'Max Martin', 'Timbaland', 'Don Was'],
+    correct: 'Rick Rubin',
+    history:
+      'Rubin took the RHCP\'s messy sound into more melodic territory, lashing their humping bass lines to fat metal riffs and the crackling rhythms of hip-hop.',
+  },
+  {
+    question: "What was RHCP's first #1 album in the US?",
+    answers: [
+      'Californication',
+      'Mother\'s Milk',
+      'By The Way',
+      'Stadium Arcadium',
+    ],
+    correct: 'Stadium Arcadium',
+    history:
+      'Stadium Arcadium is the ninth studio album by American rock band Red Hot Chili Peppers. In the U.S., Stadium Arcadium became the band\'s first number one selling album.',
+  },
+];
